d4e24521a50cc1abb4e9a71b66008dd0667a1a1b
jaehun <nojahoon@naver.com>
2022-07-27 13:49:30 +0900

    Final section5 completed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-
+const db = require('./queries')
 app.use(bodyParser.json())
 
 app.use(
@@ -15,6 +15,13 @@ app.use(
 app.get('/',(request,response) => {
   response.json({Welcome: 'How to create API with Node.js , EXPRESS and PostgreSQL'})
 })
+
+// Endpoints
+app.get('/countries',db.getCountries)
+app.get('/countries/:id',db.getCountryById)
+app.post('/countries',db.createCountry)
+app.put('/countries/:id',db.updateCountry)
+app.delete('/countries/:id',db.deleteCountry)
 
 app.listen(port , () => {
   console.log(`App running on port ${port}. `)
